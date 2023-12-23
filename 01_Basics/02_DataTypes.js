a959816fe835863443d7262963d5c192f9aaa561
Sabin Nepali <ersabin123@gmail.com>
2023-12-23 17:36:53 +0530

    DataTypes

---
+++ 01_Basics/02_DataTypes.js
@@ -0,0 +1,17 @@
+"use strict" // Treat all JS code as newer version
+
+let name = "Sabin";
+let age = 23;
+let bankAccountNumber = 2207010065679;
+let isMarried = false;
+let wife = null;
+let girlFriend;
+
+console.log(typeof(name));
+console.log(typeof(age));
+console.log(typeof(bankAccountNumber));
+console.log(typeof(isMarried));
+console.log(typeof(wife));
+console.log(typeof(girlFriend));
+
+/* Type of Null is Object */
