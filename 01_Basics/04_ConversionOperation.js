adb013c956280058ff9bc5bea6d1d53741e0a68e
Sabin Nepali <ersabin123@gmail.com>
2023-12-23 20:00:42 +0530

    Conversion Operation

---
+++ 01_Basics/04_ConversionOperation.js
@@ -0,0 +1,20 @@
+/********** Operations **********/
+
+let value = 69;
+let negValue = -value;
+console.log(value);
+console.log(negValue);
+
+let firstName = "Sabin";
+let lastName = "Nepali";
+
+let fullName = firstName + " " + lastName;
+
+console.log(fullName);
+
+console.log("1" + 2);  // 12
+console.log(1 + "2");  // 12
+console.log("1" + 2 + 2);  // 122
+console.log(1 + 2 + "2");  // 32
+
+/* Jo first mein aata hai wohi type rehta hai */
