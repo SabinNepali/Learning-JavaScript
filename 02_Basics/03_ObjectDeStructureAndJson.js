5916b284fe5973374a9f71056a8e0775a2f09995
Sabin Nepali <sabin.nepali@cginfinity.com>
2023-12-27 17:33:10 +0530

    Object De-structure and Json

---
+++ 02_Basics/03_ObjectDeStructureAndJson.js
@@ -0,0 +1,20 @@
+/*********************************OBJECT DE-STRUCTURE****************************************************/
+
+const course = {
+    course: "BE in Computer Science",
+    price: "65000",
+    institution: "Chitkara University"
+}
+
+const {institution} = course;  // De-structuring object
+
+console.log(institution);
+
+/*************************************JSON************************************
+ * JSON data example
+    {            
+        "name": "Sabin",
+        "age": 22,
+        "designation": "Associate Software Engineer"
+    }
+*/
